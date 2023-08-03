bcba89194002fd535e59a4ce1739c201c7e31816
Ankan Sadhukhan <sadhukhanankan7@gmail.com>
2023-08-04 00:46:35 +0530

    added

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+
+const Header=()=>{
+
+    const [btnName,sebtnName]=useState("Login")
+
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={LOGO_URL}></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <button className="login" onClick={() =>{
+                        btnName==="Login"?sebtnName("Logout") : sebtnName("Login")
+                    }}>{btnName}</button>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
